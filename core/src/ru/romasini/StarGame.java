4a498270f4e0f9b7f6c80e6c31328283fdcf43d2
Roman Sinitsyn <romasini@gmail.com>
2020-04-26 14:27:59 +0300

    домашнее задание 1

--- core/src/ru/romasini/StarGame.java
+++ core/src/ru/romasini/StarGame.java
@@ -9,13 +9,13 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture backScreen;
 	TextureRegion region;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		backScreen = new Texture("backScreenSpace.jpg");
 	}
 
 	@Override
@@ -23,13 +23,13 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0,0);
+		batch.draw(backScreen, -200,-200);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+        backScreen.dispose();
 	}
 }

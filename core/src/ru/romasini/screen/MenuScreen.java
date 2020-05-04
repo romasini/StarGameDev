b24a93eacc078f97483aa66f14c3e6a4a04e8b7a
Roman Sinitsyn <romasini@gmail.com>
2020-05-04 12:26:40 +0300

    доработка домашнее задание 2

--- core/src/ru/romasini/screen/MenuScreen.java
+++ core/src/ru/romasini/screen/MenuScreen.java
@@ -2,28 +2,43 @@ package ru.romasini.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.romasini.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
+
+    private static final float V_LEN = 0.8f;
     private Texture backScreen;
+    private Sprite backScreenSprite;
     private Texture spaceCat;
     private TextureRegion spaceCatFace;
-    private Vector2 posSpaceCat, newPosSpaceCat, velSpaceCat, deltaSpace;
+    private Vector2 posSpaceCat, newPosSpaceCat, velSpaceCat, common;
+
+
+    @Override
+    public void resize(int width, int height) {
+        backScreenSprite.setSize(width, height);
+        batch.getProjectionMatrix().setToOrtho2D(0,0,width,height);
+    }
 
     @Override
     public void show() {
         super.show();
         backScreen = new Texture("backScreenSpace.jpg");
+        backScreenSprite = new Sprite(backScreen);
+        backScreenSprite.setSize(Gdx.graphics.getWidth(),  Gdx.graphics.getHeight());
+        backScreenSprite.setPosition(0,0);
+
         spaceCat = new Texture("spaceCat.jpg");
-        posSpaceCat = new Vector2(50,50);
         spaceCatFace = new TextureRegion(spaceCat, 150,270,250,200 );
+
+        posSpaceCat = new Vector2(50,50);
         newPosSpaceCat = new Vector2(posSpaceCat);
         velSpaceCat = new Vector2(0,0);
-        deltaSpace = new Vector2(0,0);
-
+        common = new Vector2(0,0);
     }
 
 
@@ -31,34 +46,24 @@ public class MenuScreen extends BaseScreen {
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(backScreen, -200,-200);
-        if(!stopMove())
+        batch.draw(backScreenSprite, -100,-100);
+        if (common.set(newPosSpaceCat).sub(posSpaceCat).len2() > velSpaceCat.len2())
             posSpaceCat.add(velSpaceCat);
+        else
+            posSpaceCat.set(newPosSpaceCat);
         batch.draw(spaceCatFace, posSpaceCat.x, posSpaceCat.y, 150, 120);
         batch.end();
     }
 
-    private void calcNewVelocity(){
-        velSpaceCat.set(newPosSpaceCat.x-posSpaceCat.x, newPosSpaceCat.y-posSpaceCat.y).nor();
-    }
-
-    private boolean stopMove(){
-        deltaSpace.set(newPosSpaceCat.x-posSpaceCat.x, newPosSpaceCat.y-posSpaceCat.y);
-        if(velSpaceCat.len2() > 0 && velSpaceCat.len2()>= deltaSpace.len2()){
-            posSpaceCat.set(newPosSpaceCat);
-            velSpaceCat.set(0,0);
-            deltaSpace.set(0,0);
-            return true;
-        }
-        if (deltaSpace.len2()>0)
-            velSpaceCat.scl(1.05f);//boost
-        return false;
-    }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         newPosSpaceCat.set(screenX, Gdx.graphics.getHeight() - screenY);
-        calcNewVelocity();
+        velSpaceCat.
+                set(newPosSpaceCat.
+                        cpy().
+                        sub(posSpaceCat)).
+                setLength(V_LEN);
         return false;
     }
 
